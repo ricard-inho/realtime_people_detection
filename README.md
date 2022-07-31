54b5e8abc5c18cd1ad22288bfdac5dc8332d4057
Ricard Marsal I Castan <marsalcastan@gmail.com>
2022-07-31 15:31:29 +0900

    torchhub

--- README.md
+++ README.md
@@ -7,3 +7,19 @@ conda create -n acemob
 ```
 
 **Step 2.** Install pytorch
+
+**Step 3.** Conda and Pip requirements
+```
+conda install -c conda-forge opencv
+conda install -c anaconda pandas
+conda install -c conda-forge matplotlib
+```
+```
+pip install pyyaml tqdm seaborn protobuf ipython psutil
+```
+
+**Step 4.** Get weights
+```
+mkdir weights
+cd weights
+wget --no-check-certificate 'https://drive.google.com/u/0/uc?id=1gglIwqxaH2iTvy6lZlXuAcMpd_U0GCUb&export=download&confirm=t&uuid=2a0362c3-7ff7-4306-bd71-dec6669cb415' -O crowdhuman_yolov5m.pt
--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-from funcs import load_model
+import torch
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='weights/crowdhuman_yolov5m.pt')
 
-model = load_model(weights='weights/crowdhuman_yolov5m.pt', map_location='cpu')
-model.eval()
+detec = model("mercury.jpeg")
 
-print(model)
+for x1,y1,x2,y2,conf,obj_type in detec.xyxy[0]:
+    print(obj_type)
+
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture('rtsp://admin:0202master1965@75.110.254.189:8554/streaming/channels/101.sdp')
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    # Our operations on the frame come here
+    gray = cv2.cv2tColor(frame, cv2.COLOR_BGR2GRAY)
+    # Display the resulting frame
+    cv2.imshow('frame', gray)
+    if cv2.waitKey(1) == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+
